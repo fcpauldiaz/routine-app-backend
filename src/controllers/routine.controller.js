b5c6cac75db5c9222f214731f6c0707d2e035954
Pablo Díaz <pablo5_diaz@hotmail.com>
2021-04-17 19:09:32 -0600

    commit

---
+++ src/controllers/routine.controller.js
@@ -0,0 +1,32 @@
+const httpStatus = require('http-status');
+const pick = require('../utils/pick');
+const ApiError = require('../utils/ApiError');
+const catchAsync = require('../utils/catchAsync');
+const { routineService } = require('../services');
+
+const createRoutine = catchAsync(async (req, res) => {
+  const routine = await routineService.createRoutine(req.body, req.user);
+  res.status(httpStatus.CREATED).send(routine);
+});
+
+const getRoutines = catchAsync(async (req, res) => {
+  const routines = await routineService.getRoutinesByUser(req.user);
+  if (!routines) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Routines not found');
+  }
+  res.send(routines);
+});
+
+const deleteRoutine = catchAsync(async (req, res) => {
+  const result = await routineService.deleteRoutine(req.params.routineId);
+  if (result.ok === 1) {
+    res.sendStatus(httpStatus.OK);
+  }
+  throw new ApiError(httpStatus.NOT_FOUND, 'Routine not found');
+});
+
+module.exports = {
+  createRoutine,
+  getRoutines,
+  deleteRoutine,
+};
---
+++ src/validations/user.validation.js
@@ -0,0 +1,29 @@
+const Joi = require('joi');
+const { password, objectId } = require('./custom.validation');
+
+const createUser = {
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().custom(password),
+  }),
+};
+
+const getUsers = {
+  query: Joi.object().keys({
+    sortBy: Joi.string(),
+    limit: Joi.number().integer(),
+    page: Joi.number().integer(),
+  }),
+};
+
+const getUser = {
+  params: Joi.object().keys({
+    userId: Joi.string().custom(objectId),
+  }),
+};
+
+
+module.exports = {
+  createUser,
+  getUser,
+};
