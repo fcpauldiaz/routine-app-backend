3fb86e397d775ac9435e8d6b3054ac00b3815b8d
Pablo Díaz <pablo5_diaz@hotmail.com>
2021-04-17 18:21:45 -0600

    Revert "first commit"
    This reverts commit 0576c8a58f709e2a61774cb6a65e8f6ef75d3cd2.

--- src/services/index.js
+++
@@ -1,4 +0,0 @@
-module.exports.authService = require('./auth.service');
-module.exports.tokenService = require('./token.service');
-module.exports.userService = require('./user.service');
-module.exports.routineService = require('./routine.service');
