83728bd14e0868668586a14136280e7f07f90c0e
Pablo Díaz <pablo5_diaz@hotmail.com>
2021-04-17 23:25:25 -0600

    Change time datetime to string

--- src/models/routine.model.js
+++ src/models/routine.model.js
@@ -12,7 +12,7 @@ const routineSchema = mongoose.Schema(
     tasks: [
       {
         type: { type: String },
-        time: { type: Date },
+        time: { type: String },
       },
     ],
     user: {
